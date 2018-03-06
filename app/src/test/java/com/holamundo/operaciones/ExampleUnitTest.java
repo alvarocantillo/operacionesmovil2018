93ea7773385983051beedeadec66d013094d3b5c
AlvaroCantillo <alvjc225@gmail.com>
2018-03-05 21:25:24 -0500

    new 2

--- app/src/test/java/com/holamundo/operaciones/ExampleUnitTest.java
+++ app/src/test/java/com/holamundo/operaciones/ExampleUnitTest.java
@@ -13,5 +13,32 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
     }
+    @Test
+    public void sumar_los_numeros_correctamente(){
+        double a=4, b=5;
+        double res=Metodos.sumar(a,b);
+        assertEquals(9,res, 0);
+    }
+    @Test
+    public void restar_los_numeros_correctamente(){
+        double a=4, b=5;
+        double res=Metodos.restar(a,b);
+        assertEquals(-1,res, 0);
+    }
+    @Test
+    public void multiplicar_los_numeros_correctamente(){
+        double a=4, b=5;
+        double res=Metodos.multiplicar(a,b);
+        assertEquals(20,res, 0);
+    }
+    @Test
+    public void dividir_los_numeros_correctamente(){
+        double a=4, b=2;
+        double res=Metodos.dividir(a,b);
+        assertEquals(2,res, 0);
+    }
+
+
 }
