3e064a2dd09e3a1d9f700084f5057acbb5e8e5a6
AlvaroCantillo <alvjc225@gmail.com>
2018-03-05 20:35:16 -0500

    new

---
+++ app/src/main/java/com/holamundo/operaciones/Metodos.java
@@ -0,0 +1,21 @@
+package com.holamundo.operaciones;
+
+/**
+ * Created by Andres "Bara" Martinez on 26/02/2018.
+ */
+
+public class Metodos {
+
+    public static double sumar(double n1 , double n2){
+        return n1+n2;
+    }
+    public static double restar(double n1 , double n2){return n1+n2;}
+    public static double multiplicar(double n1 , double n2){
+        return n1+n2;
+    }
+    public static double dividir(double n1 , double n2){
+        return n1+n2;
+    }
+
+
+}
